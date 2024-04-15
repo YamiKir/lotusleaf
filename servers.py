8784be35a363c8bbe65970fe8398320e759ad06b
YamiKir <102172354+YamiKir@users.noreply.github.com>
2024-04-15 01:06:50 -0500

    Update servers.py
    Now reads the tracker information from "setup.txt".  Sends all the connected clients to every node and sleeps for 2 seconds afterwards.
    ======
    Setup.txt
    ======
    Tracker: {IPv4}
    Port: {Port}

--- servers.py
+++ servers.py
@@ -1,19 +1,43 @@
 import socket
 import threading
+import json
+import time
 
 ## things needed to be added to Server class: 
 #  tracking of what files are on which clients 
 #  connecting clients together 
 #  tracking the status of clients 
 ## 
-
+def read_setup(file_name):
+    try:
+        with open(file_name, 'r') as file:
+            setup_info = {}
+            for line in file:
+                if line.startswith("Tracker:"):
+                    tracker_ip = line.split("Tracker:")[1].strip()
+                    setup_info['Tracker IP'] = tracker_ip
+                elif line.startswith("Port:"):
+                    port = int(line.split("Port:")[1].strip())
+                    setup_info['Port'] = port
+            return setup_info
+        print("Tracker IP or Port not found in the file.")
+        return None
+    except FileNotFoundError:
+        print("File not found.")
+        return None
+    
 class Server:
 
-    MAX_WAITING=3 ##var for the max number of waiting connections
-
+    MAX_WAITING=3 ##var for the max number of waiting connection
+    def print_connections(self):
+        print("Active Connections:")
+        for connection in self.connections:
+            print(connection)
+        
     def __init__(self,host,port):
-        self.host=host
-        self.port=port
+        setup_info=read_setup("setup.txt")
+        self.host= setup_info['Tracker IP']
+        self.port=setup_info['Port']
         self.connections=[]
         self.server_socket=None
 
@@ -22,30 +46,38 @@ class Server:
         self.server_socket.bind((self.host, self.port)) ##sets the server socket
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) ##allows for the same socket to be connected again
         self.server_socket.listen(self.MAX_WAITING) ##allows for number of waiting connection 
-        print(f"Server started on {self.host}:{self.port}")
+        print("Server started on {}:{}".format(self.host, self.port))
 
         while True:
             client_socket, client_address = self.server_socket.accept() ## socket.accept() returns the socket and address information of the accepted socket.
-            print(f"Connection from {client_address}")
+            print("Connection from {}.".format(client_address))
             self.connections.append(client_socket) ## adds the current connection to the connections list
             client_thread = threading.Thread(target=self.handle_client, args=(client_socket,)) ##please please please dont elimate the ,. (needed as Thread expects a tuple)
             client_thread.start()
     def handle_client(self, client_socket):
-        print(f"Handling client @ {client_socket}")
+        print("Handling client @ {}".format(client_socket))
         while True:
             try:
-                message = input("Enter message to send to client: ")  # Get message from user
-                client_socket.send(message.encode())  # Send message to client
+                ##message = input("Enter message to send to client: ")  # Get message from user
+                ##client_socket.send(message.encode())  # Send message to client
+                connection_list = [str(addr.getpeername()) for addr in self.connections]
+                connection_list_json = json.dumps(connection_list)
+                time.sleep(5)
+                client_socket.send(connection_list_json.encode())
+                self.print_connections() ## development line
             except ConnectionResetError:
                 print("Connection closed by the client.")
                 self.connections.remove(client_socket)
                 break
+   
 
 def main ():
-
+    try:
         print("This is the main function in this Python P2P Program. (Server Edition)")
-        server=Server('localhost',9999)
+        server=Server('169.254.132.175',80)
         server.start()
+    except Exception as e:
+        print("An error occurred: " + str(e))
         
 
 if __name__ == "__main__":
