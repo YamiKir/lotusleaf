474ac87a9dc0e9fbb84c38b9d9f6bd962ff6a901
YamiKir <102172354+YamiKir@users.noreply.github.com>
2024-04-29 22:30:29 -0500

    Update servers.py

--- servers.py
+++ servers.py
@@ -35,8 +35,9 @@ class Server:
 
     def start(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        
         self.server_socket.bind((self.host, self.port))
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server_socket.listen(self.MAX_WAITING)
         print("Server started on {}:{}".format(self.host, self.port))
 
