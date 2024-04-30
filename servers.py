8ae7734841717d120d36adbc2b03a11068dd9695
YamiKir <102172354+YamiKir@users.noreply.github.com>
2024-04-29 20:53:06 -0500

    Update servers.py
    now sends ip and file list instead of sockets

--- servers.py
+++ servers.py
@@ -3,11 +3,6 @@ import threading
 import json
 import time
 
-## things needed to be added to Server class: 
-#  tracking of what files are on which clients 
-#  connecting clients together 
-#  tracking the status of clients 
-## 
 def read_setup(file_name):
     try:
         with open(file_name, 'r') as file:
@@ -26,68 +21,60 @@ def read_setup(file_name):
         print("File not found.")
         return None
     
+# Define the Server class
 class Server:
+    MAX_WAITING = 3  # Max number of waiting connections
 
-    MAX_WAITING=3 ##var for the max number of waiting connection
-    def print_connections(self):
-        print("Active Connections:")
-        for connection in self.connections:
-            print(connection)
-        
-    def __init__(self,host,port):
-        setup_info=read_setup("setup.txt")
-        self.host= setup_info['Tracker IP']
-        self.port=setup_info['Port']
-        self.connections=[]
-        self.server_socket=None
+    def __init__(self):
+        setup_info = read_setup("setup.txt")
+        self.host = setup_info['Tracker IP']
+        self.port = setup_info['Port']
+        self.connections = []
+        self.files_per_client = {}  # Dictionary to store files per client
+        self.server_socket = None
 
     def start(self):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) ##AF_INET=>IP4, SOCK_STREAM =>TCP (so that they continue to comminucate after a reply) (INET6 is IP6)
-        self.server_socket.bind((self.host, self.port)) ##sets the server socket
-        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1) ##allows for the same socket to be connected again
-        self.server_socket.listen(self.MAX_WAITING) ##allows for number of waiting connection 
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.server_socket.bind((self.host, self.port))
+        self.server_socket.listen(self.MAX_WAITING)
         print("Server started on {}:{}".format(self.host, self.port))
 
         while True:
-            client_socket, client_address = self.server_socket.accept() ## socket.accept() returns the socket and address information of the accepted socket.
+            client_socket, client_address = self.server_socket.accept()
             print("Connection from {}.".format(client_address))
-            self.connections.append(client_socket) ## adds the current connection to the connections list
-            client_thread = threading.Thread(target=self.handle_client, args=(client_socket,)) ##please please please dont elimate the ,. (needed as Thread expects a tuple)
+            self.connections.append(client_socket)
+            client_thread = threading.Thread(target=self.handle_client, args=(client_socket,))
             client_thread.start()
+
     def handle_client(self, client_socket):
-        #print("Handling client @ {}".format(client_socket))
-        # Receive file list from client upon connection
-        file_list_data = client_socket.recv(4096).decode()
-        file_list = json.loads(file_list_data)
-        print("Received file list from client:")
-        for file_name, file_size in file_list:
-            print("{} - {} bytes".format(file_name, file_size))
+        data = client_socket.recv(4096)
+        if data:
+            data_str = data.decode()
+            received_data = json.loads(data_str)
+            ip = received_data['ip']
+            files_list = received_data['files']
+            self.files_per_client[ip] = files_list  # Update files_per_client dictionary
+            print("Received file list from {}: {}".format(ip, files_list))
 
         while True:
             try:
-                ##message = input("Enter message to send to client: ")  # Get message from user
-                ##client_socket.send(message.encode())  # Send message to client
-                connection_list = [str(addr.getpeername()) for addr in self.connections]
-                connection_list_json = json.dumps(connection_list)
+                # Send files_per_client to client
+                files_per_client_json = json.dumps(self.files_per_client)
+                client_socket.send(files_per_client_json.encode())
                 time.sleep(5)
-                client_socket.send(connection_list_json.encode())
-                #self.print_connections() ## development line
-            except ConnectionResetError:
+            except socket.error:
                 print("Connection closed by the client.")
                 self.connections.remove(client_socket)
                 break
-   
 
-def main ():
-    try:
-        print("This is the main function in this Python P2P Program. (Server Edition)")
-        server=Server('169.254.132.175',80)
-        server.start()
-    except Exception as e:
-        print("An error occurred: " + str(e))
-        
+# Main function
+def main():
+    print("This is the main function in this Python P2P Program. (Server Edition)")
+    server = Server()
+    server.start()
 
 if __name__ == "__main__":
     main()
-    while True: ## so window doesnt disappear
+    while True:
         pass
